deff89852d4ffed0e54f58f6cecef1d939a485ea
Makhnev Konstantin <mah.kons@gmail.com>
2020-04-22 16:16:44 +0300

    UI first impl

---
+++ Application.py
@@ -0,0 +1,71 @@
+from tkinter import Tk, StringVar, Entry, Frame, Text, LEFT, RIGHT, END, X, Y, BOTTOM, BOTH, INSERT, Scrollbar, Listbox, Button, simpledialog
+from tkinter import N, S, W, E
+
+import argparse
+
+
+class ChatWindow:
+    def __init__(self, root, username):
+        self.frame = Frame(root)
+        self.username = username
+
+        self.scrollbar = Scrollbar(self.frame)
+        self.messages = Listbox(self.frame, yscrollcommand=self.scrollbar.set, height=15, width=50)
+        self.scrollbar.grid(row=0, column=1, sticky=N+S)
+        self.messages.grid(row=0, column=0)
+        self.scrollbar.config(command = self.messages.yview)
+
+        self.input_user = StringVar()
+        self.input_field = Entry(self.frame, text=self.input_user)
+        self.input_field.grid(row=1, column=0, sticky=W+E)
+
+        self.send_button = Button(self.frame, text="Send", command=self.send_message)
+        self.send_button.grid(row=1, column=1)
+
+        self.input_field.bind("<Return>", lambda key: self.send_message())
+        self.frame.pack()
+
+    def send_message(self):
+        input_val = self.input_field.get() 
+        if input_val == "":
+            return
+        if True:
+            self.put_message_in_chat(self.username + ": " + input_val)
+            self.input_user.set("")
+            self.messages.yview(END)
+
+
+    def put_message_in_chat(self, message):
+        self.messages.insert(END, message)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', type=str, required=False)
+    parser.add_argument('--port', type=str, required=False)
+    return parser
+
+
+def client(address, port):
+    root = Tk()
+    root.title("Chat")
+    root.withdraw()
+    username = simpledialog.askstring("Username", "What's your name", parent=root)
+    root.deiconify()
+
+    chat_window = ChatWindow(root, username)
+    root.mainloop()
+
+def server():
+    pass
+
+if __name__ == "__main__":
+    args = create_parser().parse_args()
+    if args.address is None and args.port is None:
+        server()
+    elif args.address is None or args.port is None:
+        print("you should specify both address and port or none of them")
+    else:
+        client(args.address, args.port)
+
+
